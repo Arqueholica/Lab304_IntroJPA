00c9cd3e18619dbc9e7b0579f04708b74be90df9
Ludmila S. Leboso Sánchez <ludmilaleboso@hotmail.com>
2025-09-01 21:58:55 +0200

    EnumOK¿?

--- Lab304_IntroJPA/src/main/java/com/ironhack/Lab304_IntroJPA/models/Customer.java
+++ Lab304_IntroJPA/src/main/java/com/ironhack/Lab304_IntroJPA/models/Customer.java
@@ -1,9 +1,15 @@
 package com.ironhack.Lab304_IntroJPA.models;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 @Entity
 public class Customer {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Integer customerId;
+private String customerName;
 
+@Enumerated(EnumType.STRING)
+private CustomerStatus customerStatus;
 
 }
