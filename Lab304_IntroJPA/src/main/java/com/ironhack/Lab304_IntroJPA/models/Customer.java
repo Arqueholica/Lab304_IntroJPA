d8af3bf197f61c3d382a82ae9acc624d02bc783d
Ludmila S. Leboso Sánchez <ludmilaleboso@hotmail.com>
2025-09-01 22:05:00 +0200

    CustomerOK

--- Lab304_IntroJPA/src/main/java/com/ironhack/Lab304_IntroJPA/models/Customer.java
+++ Lab304_IntroJPA/src/main/java/com/ironhack/Lab304_IntroJPA/models/Customer.java
@@ -8,8 +8,54 @@ public class Customer {
 @GeneratedValue(strategy = GenerationType.IDENTITY)
 private Integer customerId;
 private String customerName;
+private Integer totalCustomerMileage;
 
 @Enumerated(EnumType.STRING)
 private CustomerStatus customerStatus;
 
+// CONSTRUCTORES
+
+    public Customer() {
+    }
+
+    public Customer(String customerName, Integer totalCustomerMileage, CustomerStatus customerStatus) {
+        this.customerName = customerName;
+        this.totalCustomerMileage = totalCustomerMileage;
+        this.customerStatus = customerStatus;
+    }
+
+    // GETTERS&SETTERS
+
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public Integer getTotalCustomerMileage() {
+        return totalCustomerMileage;
+    }
+
+    public void setTotalCustomerMileage(Integer totalCustomerMileage) {
+        this.totalCustomerMileage = totalCustomerMileage;
+    }
+
+    public CustomerStatus getCustomerStatus() {
+        return customerStatus;
+    }
+
+    public void setCustomerStatus(CustomerStatus customerStatus) {
+        this.customerStatus = customerStatus;
+    }
 }
